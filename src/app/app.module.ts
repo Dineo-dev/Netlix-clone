98a26d16994f5c3a6df4e22df2e84bb653d9b787
Dineo <dineo.phahla@dadevs.co.za>
2023-05-05 10:08:31 +0200

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { ServicesComponent } from './services/services.component';
+import { CardServiceComponent } from './services/card-service/card-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CardComponent,
+    ServicesComponent,
+    CardServiceComponent
   ],
   imports: [
     BrowserModule,
