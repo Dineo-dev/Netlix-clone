d9894d07f810703e5cc823e2f5155e5e1235f645
Dineo <dineo.phahla@dadevs.co.za>
2023-05-08 12:30:04 +0200

    components done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { CardComponent } from './components/card/card.component';
 import { LatestMoviesComponent } from './components/latest-movies/latest-movies.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { BackButtonComponent } from './components/back-button/back-button.component';
+import { SuggestuonPageComponent } from './components/suggestuin-page/suggestuon-page.component';
+import { SuggestionPageComponent } from './components/suggestion-page/suggestion-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { OnViewComponent } from './components/on-view/on-view.component';
+import { SplashComponent } from './components/splash/splash.component';
 // import { ServicesComponent } from './services/services.component';
 // import { CardServiceComponent } from './services/card-service/card-service.component';
 
@@ -19,6 +25,12 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
     LatestMoviesComponent,
     SignInComponent,
     SignUpComponent,
+    BackButtonComponent,
+    SuggestuonPageComponent,
+    SuggestionPageComponent,
+    ErrorPageComponent,
+    OnViewComponent,
+    SplashComponent,
     // ServicesComponent,
     // CardServiceComponent
   ],
---
+++ src/app/components/on-view/on-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OnViewComponent } from './on-view.component';
+
+describe('OnViewComponent', () => {
+  let component: OnViewComponent;
+  let fixture: ComponentFixture<OnViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OnViewComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OnViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
